8b33f1e469bd4aaec006a1840f23568ff431b611
Cabri Chamberlin <cabriamber@gmail.com>
2023-06-26 21:19:42 -0600

    Add functionality to color scheme buttons

--- script.js
+++ script.js
@@ -14,8 +14,18 @@ const monochrome = document.getElementById("monochrome");
 const desert = document.getElementById("desert"); 
 const cottonCandy = document.getElementById("cottonCandy");
 const peacock = document.getElementById("peacock"); 
+const vaporwave = document.getElementById("vaporwave");
+
+const royalNights = document.getElementById("royalNights");
+const planetUtopia = document.getElementById("planetUtopia"); 
+const summerPark = document.getElementById("summerPark"); 
+const judysFlowerGarden = document.getElementById("judysFlowerGarden"); 
+const opalPrism = document.getElementById("opalPrism"); 
+
+
 const eraser = document.getElementById("eraser"); 
 const clearButton = document.getElementById("clearButton"); 
+
 
 let colorScheme = "black";
 
@@ -34,7 +44,31 @@ cottonCandy.addEventListener("click", () => console.log(colorScheme));
 peacock.addEventListener("click", () => colorScheme = "peacock"); 
 peacock.addEventListener("click", () => console.log(colorScheme));
 
+vaporwave.addEventListener("click", () => colorScheme = "vaporwave"); 
+
+royalNights.addEventListener("click", () => colorScheme = "royalNights"); 
+royalNights.addEventListener("click", () => console.log(colorScheme)); 
+
+planetUtopia.addEventListener("click", () => colorScheme = "planetUtopia"); 
+planetUtopia.addEventListener("click", () => console.log(colorScheme)); 
+
+summerPark.addEventListener("click", () => colorScheme = "summerPark"); 
+summerPark.addEventListener("click", () => console.log(colorScheme)); 
+
+judysFlowerGarden.addEventListener("click", () => colorScheme = "judysFlowerGarden"); 
+judysFlowerGarden.addEventListener("click", () => console.log(colorScheme)); 
+
+opalPrism.addEventListener("click", () => colorScheme = "opalPrism"); 
+opalPrism.addEventListener("click", () => console.log(colorScheme)); 
+
+
+
+
+
+
+
 eraser.addEventListener("click", () => colorScheme = "eraser");
+
 clearButton.addEventListener("click", eraseAll)
 
 function eraseAll() {
@@ -60,10 +94,23 @@ let monochromeColors = ["#E3E8EA", "#BCCAD0", "#9BA8AE", "#707A7E", "#495054", "
 // all grey, from lightest to darkest 
 let cottonCandyColors = ["#FFB3DE", "#CCEFE3", "#EBC8F3", "#C9FDFF", "#FFFCC9", "#FBBED8"];
 //1)light hot pink 2)light minty green 3) easter egg purple 4) super light blue 5) baby yellow 6) another light pink 
-let peacockColors = ['#C0F862',"#10a690",'#1166A3','#3d4987','402b6d','#5b066b']
+let peacockColors = ['#C0F862',"#10a690",'#1166A3','#3d4987','#402b6d','#5b066b']
 // 1)bright yellow green 2) greenish blue 3) teal 4)purple blue 4)royal purple 6) burnt yellow orange
-let sunsetColors = ["#300350","#94167F","#E93479","F9AC53","#F62E97","#153CB4"]
+let vaporwaveColors = ["#300350","#94167F","#E93479","#F9AC53","#F62E97","#153CB4"]
 //1)Russian Violet 2)MardiGras Dark Pink 3)Cerise Light Pink 4) Rajah (sand color) 5) Persian Rose (bright pink) 6) Persian Blue (close to royal blue)
+let royalNightsColors = ["#B72D0F","#E0C020","#8D38AF","#F18730","#2A60A4","#8C8181"]; 
+//Drew's colors 
+let planetUtopiaColors = ["#FCB300","#f7072f","#19a2f6","#f6f619","#e843e8","#66e843"];
+// Scott's Colors 
+let judysFlowerGardenColors = ["#ef3e1b","#ef1b58","#efec1b","#9f1bef","#1bef52","#114aef"];
+//Mom's Colors
+let summerParkColors =["#e00081","#e05500","#dde000","#e03a00","#e03a00","#e00000"];
+//Nyla's Colors 
+let opalPrismColors = ["#a98cda","#7937e5","#6309f5","#cfc4e1","#5d2fa8","#4d0bba"];
+//Tiya's Colors 
+let hauntedForestColors = ["#d5d8d8","#69896d","#3e625c","#1d443e","#0f292f","#196f3d"];
+let corporateSuit = ["#ede8da","#c1bbaf","#8e8c8a","#534e50","#212736","#34495e"];
+
 
 
 function colorChange(e) {
@@ -91,9 +138,31 @@ function colorChange(e) {
     console.log(peacockColors[randomNumber])
 
   }
+  else if (colorScheme === "vaporwave") {
+    e.target.style.backgroundColor = `${vaporwaveColors[randomNumber]}`; 
+    console.log(randomNumber);
+    console.log(vaporwaveColors[randomNumber])
+  }
+  else if (colorScheme === "royalNights") {
+    e.target.style.backgroundColor = `${royalNightsColors[randomNumber]}`; 
+    console.log(royalNightsColors[randomNumber]); 
+  }
+  else if (colorScheme === "planetUtopia") {
+    e.target.style.backgroundColor = `${planetUtopiaColors[randomNumber]}`; 
+  }
+  else if (colorScheme === "judysFlowerGarden") {
+    e.target.style.backgroundColor = `${judysFlowerGardenColors[randomNumber]}`; 
+  }
+  else if (colorScheme === "summerPark") {
+    e.target.style.backgroundColor = `${summerParkColors[randomNumber]}`; 
+  }
+  else if (colorScheme === "opalPrism") {
+    e.target.style.backgroundColor = `${opalPrismColors[randomNumber]}`; 
+  }
   else if (colorScheme === "eraser") {
     e.target.style.backgroundColor = "white"; 
   }
+  
 }
 
 function setUpGrid(size) {
